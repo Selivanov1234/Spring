971403bd0a435547ab31aa768440d696f553f7d7
Sergey Selivanov <s.i.selivanov@gmail.com>
2022-04-25 17:41:31 +0300

    Lesson 4 user + delete

--- lesson-04-spring-boot/lesson-04-spring-boot/src/main/java/ru/geekbrains/lesson04springboot/controller/UserController.java
+++ lesson-04-spring-boot/lesson-04-spring-boot/src/main/java/ru/geekbrains/lesson04springboot/controller/UserController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.lesson04springboot.persist.User;
 import ru.geekbrains.lesson04springboot.persist.UserRepository;
 
@@ -53,6 +50,12 @@ public class UserController {
             return "user_form";
         }
         userRepository.save(user);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable long id) {
+        userRepository.delete(id);
         return "redirect:/user";
     }
 }
